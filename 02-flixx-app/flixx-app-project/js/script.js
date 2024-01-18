0cb2a637233e856d919ce252c72859210c74fda3
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-01-18 10:58:42 +0600

    Flix app: get and display popular movies

--- 02-flixx-app/flixx-app-project/js/script.js
+++ 02-flixx-app/flixx-app-project/js/script.js
@@ -7,7 +7,7 @@ function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
-      console.log("home");
+      displayPopularMovies();
       break;
     case "/shows.html":
       console.log("Shows");
@@ -34,6 +34,58 @@ function highlightActiveLink() {
     if (link.getAttribute("href") === global.currentPage) {
       link.classList.add("active");
     }
+  });
+}
+
+// Fetch data from API
+async function fetchAPIData(endpoint) {
+  const API_KEY = "0df4d8122ee146e4718616c0bfd1b410";
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  return data;
+}
+
+// Display popular movies
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+
+  // console.log(results);
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+      ${
+        movie.poster_path
+          ? `<img
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+      />`
+          : `<img
+      src="../images/no-image.jpg"
+      class="card-img-top"
+      alt="${movie.title}"
+    />`
+      }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${movie.title}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${movie.release_date}</small>
+      </p>
+    </div>
+  `;
+
+    document.querySelector("#popular-movies").appendChild(div);
   });
 }
 
