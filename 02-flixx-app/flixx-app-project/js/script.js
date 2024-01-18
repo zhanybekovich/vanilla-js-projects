5ff0dd7f6f98de0b273168d56bad3390f12f0ece
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-01-18 11:01:47 +0600

    Flix app: display popular tv-shows

--- 02-flixx-app/flixx-app-project/js/script.js
+++ 02-flixx-app/flixx-app-project/js/script.js
@@ -10,7 +10,7 @@ function init() {
       displayPopularMovies();
       break;
     case "/shows.html":
-      console.log("Shows");
+      displayPopularTVShows();
       break;
     case "/movie-details.html":
       console.log("Movie details");
@@ -42,11 +42,15 @@ async function fetchAPIData(endpoint) {
   const API_KEY = "0df4d8122ee146e4718616c0bfd1b410";
   const API_URL = "https://api.themoviedb.org/3/";
 
+  showSpinner();
+
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
 
   const data = await response.json();
+
+  hideSpinner();
 
   return data;
 }
@@ -86,6 +90,54 @@ async function displayPopularMovies() {
   `;
 
     document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+
+// Show spinner function
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+
+// Hide spinner
+function hideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
+}
+
+// Display popular tv-shows
+async function displayPopularTVShows() {
+  const { results } = await fetchAPIData("tv/popular");
+
+  // console.log(results);
+
+  results.forEach((show) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+
+    div.innerHTML = `
+    <a href="tv-details.html?id=${show.id}">
+      ${
+        show.poster_path
+          ? `<img
+        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+        class="card-img-top"
+        alt="${show.name}"
+      />`
+          : `<img
+      src="../images/no-image.jpg"
+      class="card-img-top"
+      alt="${show.name}"
+    />`
+      }
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${show.name}</h5>
+      <p class="card-text">
+        <small class="text-muted">Air Date: ${show.first_air_day}</small>
+      </p>
+    </div>
+  `;
+
+    document.querySelector("#popular-shows").appendChild(div);
   });
 }
 
