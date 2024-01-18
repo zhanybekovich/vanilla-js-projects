572d5a39683f28f4819b2edba98e1f967320974a
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-01-18 11:08:02 +0600

    Flix app: tv-show detail page

--- 02-flixx-app/flixx-app-project/js/script.js
+++ 02-flixx-app/flixx-app-project/js/script.js
@@ -16,7 +16,7 @@ function init() {
       displayMovieDetails();
       break;
     case "/tv-details.html":
-      console.log("TV details");
+      displayShowDetails();
       break;
     case "/search.html":
       console.log("Search");
@@ -237,6 +237,73 @@ function displayBackgroundImage(type, backgroundPath) {
   } else {
     document.querySelector("#show-details").appendChild(overlayDiv);
   }
+}
+
+// Display show details
+async function displayShowDetails() {
+  const showID = window.location.search.split("=")[1];
+
+  const show = await fetchAPIData(`tv/${showID}`);
+
+  // Overlay for bg Images
+  displayBackgroundImage("tv", show.backdrop_path);
+
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <div class="details-top">
+  <div>
+  ${
+    show.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+    class="card-img-top"
+    alt="${show.name}"
+  />`
+      : `<img
+  src="../images/no-image.jpg"
+  class="card-img-top"
+  alt="${show.name}"
+/>`
+  }
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${show.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+    <p>
+      ${show.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+      ${show.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+    </ul>
+    <a href="${
+      show.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Episodes:</span> ${
+      show.number_of_episodes
+    }</li>
+    <li><span class="text-secondary">Last Episode To Air:</span> ${
+      show.last_episode_to_air.name
+    }</li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">${show.production_companies
+    .map((company) => `<span>${company.name}</span>`)
+    .join("")}</div>
+</div>
+  `;
+
+  document.querySelector("#show-details").appendChild(div);
 }
 
 document.addEventListener("DOMContentLoaded", init);
