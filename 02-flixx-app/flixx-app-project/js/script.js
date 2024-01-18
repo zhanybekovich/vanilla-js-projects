2cf78356d23840cb5320fed431e5a6189c4f83b7
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-01-18 11:05:33 +0600

    Flix app: bg-overlay on detail page

--- 02-flixx-app/flixx-app-project/js/script.js
+++ 02-flixx-app/flixx-app-project/js/script.js
@@ -147,6 +147,9 @@ async function displayMovieDetails() {
 
   const movie = await fetchAPIData(`movie/${movieID}`);
 
+  // Overlay for bg Images
+  displayBackgroundImage("movie", movie.backdrop_path);
+
   const div = document.createElement("div");
   div.innerHTML = `
   <div class="details-top">
@@ -211,6 +214,29 @@ async function displayMovieDetails() {
 // Function add commas to number
 function addCommasToNumber(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// Displa bockdrop on details pages
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement("div");
+
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = "cover";
+  overlayDiv.style.backgroundPosition = "center";
+  overlayDiv.style.backgroundRepeat = "no-repeat";
+  overlayDiv.style.height = "100vh";
+  overlayDiv.style.width = "100vw";
+  overlayDiv.style.position = "absolute";
+  overlayDiv.style.top = "0";
+  overlayDiv.style.left = "0";
+  overlayDiv.style.zIndex = "-1";
+  overlayDiv.style.opacity = "0.1";
+
+  if (type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlayDiv);
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", init);
